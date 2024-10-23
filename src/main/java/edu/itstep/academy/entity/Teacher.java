ac6e517e52b19d773582bc43b5ed08806b7a2f4a
kkosstiki <kkosstiki@gmail.com>
2024-10-23 17:44:45 +0300

    add Entity

---
+++ src/main/java/edu/itstep/academy/entity/Teacher.java
@@ -0,0 +1,55 @@
+package edu.itstep.academy.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "teachers")
+public class Teacher {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    public Teacher(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public Teacher() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
