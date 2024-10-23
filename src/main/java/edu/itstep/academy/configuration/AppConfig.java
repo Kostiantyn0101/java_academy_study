fa28034720c89166df6462327b226e86c36dbabb
kkosstiki <kkosstiki@gmail.com>
2024-10-23 16:05:06 +0300

    add configurations, dependencies

---
+++ src/main/java/edu/itstep/academy/configuration/AppConfig.java
@@ -0,0 +1,58 @@
+package edu.itstep.academy.configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.context.annotation.*;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "edu.itstep.academy")
+@EnableWebMvc
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        try {
+            comboPooledDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/academy_study?useUnicode=true&serverTimezone=UTC");
+            comboPooledDataSource.setUser("root");
+            comboPooledDataSource.setPassword("admin0");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return comboPooledDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource());
+        localSessionFactoryBean.setPackagesToScan("edu.itstep.academy.entity");
+        Properties properties  = new Properties();
+        properties.setProperty("hibernate.show_sql", "true");
+        localSessionFactoryBean.setHibernateProperties(properties);
+        return localSessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(){
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactory().getObject());
+        return hibernateTransactionManager;
+    }
+}
